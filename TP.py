fa5bcc0d898e59941bb2d8bc2643c3e7ed888ebf
Xinyu Wu <Cynthia@Xinyus-MacBook.wv.cc.cmu.edu>
2019-11-24 23:37:48 -0500

    market room with icons and tables

--- TP.py
+++ TP.py
@@ -16,6 +16,7 @@ class Character(object):
         self.stand = stand
         self.hitbox = (self.x, self.y, width, height)
         self.isCollide = False
+        self.inMarket = False
 
 class Background(object):
     def __init__(self, image, name):
@@ -42,6 +43,11 @@ class PlayGame(object):
         self.createItems()
         self.itemList = [self.home, self.bank, self.market, 
                         self.workplace, self.park]
+        self.buyDesk, self.sellDesk = None, None
+        self.sellIcon, self.buyIcon, self.marketIcon = None, None, None
+        self.createMarketItems()
+        self.marketItems = [self.buyDesk, self.sellDesk, self.buyIcon, 
+                            self.sellIcon, self.marketIcon]
 
     def createCharacter(self):
         walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), 
@@ -75,6 +81,18 @@ class PlayGame(object):
                             100, 100, 'park')
         self.workplace = Item('workplace.png', 5*self.width//6, 
                                 7*self.height//12+5, 75, 100, 'workplace')
+
+    def createMarketItems(self):
+        self.buyDesk = Item('buyDesk.png', 4*self.width//6, self.height//5, 
+                            150, 150, 'buy desk')
+        self.sellDesk = Item('sellDesk.png', self.width//10, self.height//5, 
+                            150, 100, 'sell desk')
+        self.buyIcon = Item('buyIcon.png', 5*self.width//6, self.height//10, 
+                            50, 50, 'buy icon')
+        self.sellIcon = Item('sellIcon.png', self.width//6, self.height//10, 
+                            50, 50, 'sell icon')
+        self.marketIcon = Item('marketIcon.png', self.width//2, self.height//5, 
+                                50, 50, 'market icon')
 
     def detectCollision(self): #hitbox: (left top)x, y, width, height 
         if ((self.protagonist.hitbox[0] + self.protagonist.hitbox[2] > self.market.hitbox[0] and self.protagonist.hitbox[0] + self.protagonist.hitbox[2] < self.market.hitbox[0] + self.market.hitbox[2])
@@ -114,15 +132,15 @@ class PlayGame(object):
                                 self.protagonist.height)
         pygame.draw.rect(stSurface, (255, 0, 0), self.protagonist.hitbox, 2)
 
-    def drawItem(self, stSurface):
+    def drawStItem(self, stSurface):
         for item in self.itemList:
             stSurface.blit(item.image, (item.x, item.y))
             pygame.draw.rect(stSurface, (255, 0, 0), item.hitbox, 2)
 
-    def redrawGameWindow(self, stSurface, moveL, moveR, moveF, moveB):
+    def redrawStreetWindow(self, stSurface, moveL, moveR, moveF, moveB):
         stSurface.blit(self.streetBackground.image, (0, 0))
 
-        self.drawItem(stSurface)
+        self.drawStItem(stSurface)
         self.drawProtagonist(stSurface, moveL, moveR, moveF, moveB)
         
         pygame.display.update()
@@ -192,6 +210,50 @@ class PlayGame(object):
         if self.protagonist.isCollide:
             self.cancelMove(direction)
 
+    def drawMarketItem(self, marketSurface):
+        for item in self.marketItems:
+            marketSurface.blit(item.image, (item.x, item.y))
+            pygame.draw.rect(marketSurface, (255, 0, 0), item.hitbox, 2)
+
+    def redrawMarketWindow(self, marketSurface, moveL, moveR, moveF, moveB):
+        marketBgImage = pygame.image.load('marketBg.png')
+        marketBgImage = pygame.transform.scale(marketBgImage, (self.width, 
+                                                                self.height))
+        marketSurface.blit(marketBgImage, (0, 0))
+
+        self.drawMarketItem(marketSurface)
+        self.drawProtagonist(marketSurface, moveL, moveR, moveF, moveB)
+
+        pygame.display.update()
+
+    def marketDetectCollision(self):
+        self.protagonist.isCollide = False
+
+    def inMarketRunGame(self):
+        marketSurface = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Stock Market")
+        clock = pygame.time.Clock()
+
+        run = True
+        while run:
+            print("protagonist's in market")
+            clock.tick(15)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            keys = pygame.key.get_pressed()
+            (moveL, moveR, moveF, moveB) = self.detectKeyPressed(keys)
+
+            self.marketDetectCollision()
+            print('is it colliding: ', self.protagonist.isCollide)
+            self.checkDirectForCollision(moveL, moveR, moveF, moveB)
+
+            self.redrawMarketWindow(marketSurface, moveL, moveR, moveF, moveB)
+
+        pygame.quit()
+
     def runGame(self):
         pygame.init()
         stSurface = pygame.display.set_mode((self.width, self.height))
@@ -207,13 +269,16 @@ class PlayGame(object):
                 if event.type == pygame.QUIT:
                     run = False
 
+            if self.protagonist.isCollide:
+                self.inMarketRunGame()
+            
             keys = pygame.key.get_pressed()
             (moveL, moveR, moveF, moveB) = self.detectKeyPressed(keys)
             self.detectCollision()
             print('is it colliding: ', self.protagonist.isCollide)
             self.checkDirectForCollision(moveL, moveR, moveF, moveB)
                     
-            self.redrawGameWindow(stSurface, moveL, moveR, moveF, moveB)
+            self.redrawStreetWindow(stSurface, moveL, moveR, moveF, moveB)
 
         pygame.quit()
 
